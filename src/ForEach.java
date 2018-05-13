8061e64f60b1540debb5fe71ab5113cc22361fbe
Pelle <joaquinpellegrino@gmail.com>
2018-05-12 22:23:51 -0300

    foreach en java.

---
+++ src/ForEach.java
@@ -0,0 +1,12 @@
+
+public class ForEach {
+
+	public static void main(String[] args) {
+
+		int [] numbers = new int[] {1, 2, 3, 4, 5};
+		
+		for(int n: numbers)
+			System.out.println(n);
+	}
+
+}
